9bc6fd261a2400ed108087a0683bed26d0883f69
Linda <lindathehuang@gmail.com>
2020-07-22 22:17:28 -0700

    Created Mobile Version

--- stylesheets/script.js
+++ stylesheets/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    //JQuery code goes here
+    $("#sandwich").click(function(){
+        $("#page, nav, h1").toggleClass("showNav");
+    });
+
+    $("#keymoji").click(function(){
+        $("#key div").toggleClass("keyClicked");
+    });
+
+    $(window).bind("resize", function () {
+        console.log($(this).width())
+        if ($(this).width() > 600) {
+            $("#page, nav, h1").removeClass("showNav");
+            $("#key div").removeClass("keyClicked");
+        }
+    }).trigger('resize');
 })
