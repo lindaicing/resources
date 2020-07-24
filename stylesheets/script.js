97edfcd8f2273c9c32c5109f7d46bb42c34711d8
Linda <lindathehuang@gmail.com>
2020-07-23 22:34:50 -0700

    Update script.js

--- stylesheets/script.js
+++ stylesheets/script.js
@@ -17,7 +17,13 @@ $(document).ready(function(){
     //Swipeup function from http://jsfiddle.net/Q3d9H/1/
     !function(){var o=$.support.touch,e=o?"touchstart":"mousedown",i=o?"touchend":"mouseup",t=o?"touchmove":"mousemove";$.event.special.swipeupdown={setup:function(){var o=$(this);o.bind(e,function(e){var n,s=e.originalEvent.touches?e.originalEvent.touches[0]:e,u={time:(new Date).getTime(),coords:[s.pageX,s.pageY],origin:$(e.target)};function c(o){if(u){var e=o.originalEvent.touches?o.originalEvent.touches[0]:o;n={time:(new Date).getTime(),coords:[e.pageX,e.pageY]},Math.abs(u.coords[1]-n.coords[1])>10&&o.preventDefault()}}o.bind(t,c).one(i,function(e){o.unbind(t,c),u&&n&&n.time-u.time<1e3&&Math.abs(u.coords[1]-n.coords[1])>30&&Math.abs(u.coords[0]-n.coords[0])<75&&u.origin.trigger("swipeupdown").trigger(u.coords[1]>n.coords[1]?"swipeup":"swipedown"),u=n=void 0})})}},$.each({swipedown:"swipeupdown",swipeup:"swipeupdown"},function(o,e){$.event.special[o]={setup:function(){$(this).bind(e,$.noop)}}})}();
 
-    $('body').on('swipeup',function(){
+    $('nav').on('swipeup, ',function(){
         $("nav").removeClass("showNav");
     } );
+
+    $(window).bind('mousewheel', function(event) {
+        if (event.originalEvent.wheelDelta < 0) {
+            $("nav").removeClass("showNav");
+        }
+    });
 })
